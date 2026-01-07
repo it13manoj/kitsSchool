cb5e07d2953e2adf83c6c3182b323d5b9f6bae07
it13manoj <it13manoj@gmail.com>
2026-01-07 23:31:37 +0530

    udpate

--- Route/classes.js
+++
@@ -1,9 +0,0 @@
-const express =require('express')
-const Route =express.Router()
-const createcls=require('../controller/classes')
-const auth = require('../middleware/Auth')
-
-Route.post('/classes',createcls.postclasses)
-Route.get('/classes',auth,createcls.getclasses)
-
-module.exports=Route
--- Route/contacts.js
+++
@@ -1,9 +0,0 @@
-const express =require('express')
-const Route = express.Router()
-const usercontacts = require('../controller/contacts')
-
-Route.post('/contacts',usercontacts.postcontacts)
-Route.get('/contacts',usercontacts.getcontacts)
-
-
-module.exports=Route
